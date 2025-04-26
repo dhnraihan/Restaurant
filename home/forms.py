b50e4a1ef2ccc3032dc6e72c21ac76aec515b016
Dhn_Raihan <30395903+RHNRaihan@users.noreply.github.com>
2025-04-27 02:51:40 +0600

    better UI

--- home/forms.py
+++ home/forms.py
@@ -15,3 +15,23 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(max_length=200)
     message = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({
+            'class': 'w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent',
+            'placeholder': 'Your Name'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent',
+            'placeholder': 'Your Email'
+        })
+        self.fields['subject'].widget.attrs.update({
+            'class': 'w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent',
+            'placeholder': 'Subject'
+        })
+        self.fields['message'].widget.attrs.update({
+            'class': 'w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent',
+            'placeholder': 'Your Message',
+            'rows': '6'
+        })
